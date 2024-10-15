e47a4b2aab69a3764890903d3e0b911d2a09c737
mortezanjfi <mortezanajafiofficial@gmail.com>
2024-10-16 01:32:00 +0330

    add axios config

---
+++ src/services/axios-config.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
