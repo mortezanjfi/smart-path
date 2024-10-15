1628b8b085c2383317f2413a1794074ad9b1d3ca
mortezanjfi <mortezanajafiofficial@gmail.com>
2024-10-16 01:14:56 +0330

    create main components

---
+++ src/components/charts/PriceChart/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function PriceChart({}: Props) {
+  return <div>PriceChart</div>;
+}
---
+++ src/components/charts/VolumeChart/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function VolumeChart({}: Props) {
+  return <div>VolumeChart</div>;
+}
