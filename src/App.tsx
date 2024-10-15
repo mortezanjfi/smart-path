e04f18356b4eb9c164a52281b9b8a2046e3c4637
mortezanjfi <mortezanajafiofficial@gmail.com>
2024-10-16 03:10:39 +0330

    implement first version of PriceChart

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import PriceChart from "@components/charts/PriceChart";
 import "./App.css";
 
 function App() {
-  return <div className="App">Init</div>;
+  return (
+    <div className="App">
+      <h1>Init</h1>
+      <PriceChart />
+    </div>
+  );
 }
 
 export default App;
--- src/components/charts/PriceChart/index.tsx
+++ src/components/charts/PriceChart/index.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+import services from "@services";
+import { useCallback, useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 type Props = {};
 
 export default function PriceChart({}: Props) {
-  return <div>PriceChart</div>;
+  const [chartData, setChartData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await services.getPrices({
+        fsym: "BTC",
+        tsym: "USD",
+        limit: 10,
+      });
+      const data = response?.data?.Data?.Data;
+      setChartData({
+        labels: data?.map((entry: any) =>
+          new Date(entry.time * 1000).toLocaleTimeString()
+        ),
+        datasets: [
+          {
+            label: "higher",
+            data: data?.map((entry: any) => entry?.high),
+            backgroundColor: "green",
+            borderRadius: 4,
+          },
+          {
+            label: "average",
+            data: data?.map((entry: any) => (entry?.low + entry?.high) / 2),
+            backgroundColor: "yellow",
+            borderRadius: 4,
+          },
+          {
+            label: "lower",
+            data: data?.map((entry: any) => entry?.low),
+            backgroundColor: "red",
+            borderRadius: 4,
+          },
+        ],
+      });
+    } catch (error) {
+      console.error("error fetching price data", error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return <Bar data={chartData} />;
 }
