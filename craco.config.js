69fd3d887af5d73e018e2e03eaaa8fd6302e886a
mortezanjfi <mortezanajafiofficial@gmail.com>
2024-10-16 04:07:27 +0330

    add context alias to craco and ts configs

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,7 @@ module.exports = {
       "@components": path.resolve(__dirname, "src/components"),
       "@services": path.resolve(__dirname, "src/services/index.ts"),
       "@utils": path.resolve(__dirname, "src/utils/index.ts"),
+      "@context": path.resolve(__dirname, "src/context"),
     },
   },
 };
