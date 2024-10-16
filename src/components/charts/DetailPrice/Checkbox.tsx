c96001ece3792edfcfea3b584cb2bbcdcff5487c
mortezanjfi <mortezanajafiofficial@gmail.com>
2024-10-16 04:32:48 +0330

    fix name prop of Checkbox component

--- src/components/charts/DetailPrice/Checkbox.tsx
+++ src/components/charts/DetailPrice/Checkbox.tsx
@@ -9,6 +9,7 @@ type Props = {
   checked: boolean;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   color: COLORS_ENUM;
+  name: string;
 };
 
 const colors = {
@@ -17,7 +18,7 @@ const colors = {
   [COLORS_ENUM.green]: "border-green-500 checked:bg-green-500",
 };
 
-export default function DetailPrice({ color, checked, onChange }: Props) {
+export default function DetailPrice({ name, color, checked, onChange }: Props) {
   return (
     <label className="flex items-center space-x-2">
       <input
@@ -27,7 +28,7 @@ export default function DetailPrice({ color, checked, onChange }: Props) {
         onChange={onChange}
         className={`${colors[color]} w-5 h-5 appearance-none border-2 rounded-full checked:border-transparent focus:outline-none transition duration-200`}
       />
-      <span>Average</span>
+      <span>{name}</span>
     </label>
   );
 }
