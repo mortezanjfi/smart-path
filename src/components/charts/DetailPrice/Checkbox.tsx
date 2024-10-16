e0f97ef26493c02d5fed23035ae505f9e5ea9629
mortezanjfi <mortezanajafiofficial@gmail.com>
2024-10-16 04:41:02 +0330

    fix style of Checkbox component

--- src/components/charts/DetailPrice/Checkbox.tsx
+++ src/components/charts/DetailPrice/Checkbox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-enum COLORS_ENUM {
-  blue,
+export enum COLORS_ENUM {
+  red,
   yellow,
   green,
 }
@@ -13,7 +13,7 @@ type Props = {
 };
 
 const colors = {
-  [COLORS_ENUM.blue]: "border-blue-500 checked:bg-blue-500",
+  [COLORS_ENUM.red]: "border-red-500 checked:bg-red-500",
   [COLORS_ENUM.yellow]: "border-yellow-500 checked:bg-yellow-500",
   [COLORS_ENUM.green]: "border-green-500 checked:bg-green-500",
 };
@@ -23,12 +23,12 @@ export default function Checkbox({ name, color, checked, onChange }: Props) {
     <label className="flex items-center space-x-2">
       <input
         type="checkbox"
-        name="average"
+        name={name}
         checked={checked}
         onChange={onChange}
         className={`${colors[color]} w-5 h-5 appearance-none border-2 rounded-full checked:border-transparent focus:outline-none transition duration-200`}
       />
-      <span>{name}</span>
+      <span className="first-letter:uppercase">{name}</span>
     </label>
   );
 }
