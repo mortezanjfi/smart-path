4f83a5fba058e9a1b762c2f3753e06e2e507d233
mortezanjfi <mortezanajafiofficial@gmail.com>
2024-10-16 03:30:40 +0330

    update App component style with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Init</h1>
-      <PriceChart />
+    <div className="min-h-screen bg-gray-100 max-h-screen overflow-hidden flex items-center h-screen justify-center">
+      <div className="grid w-full gap-6 grid-rows-2 h-full p-8 grid-cols-3 flex-1">
+        <div className="col-span-2 bg-white rounded-md flex justify-center items-center">
+          <PriceChart />
+        </div>
+        <div className="bg-white rounded-md"></div>
+        <div className="bg-white rounded-md col-span-full"></div>
+      </div>
     </div>
   );
 }
